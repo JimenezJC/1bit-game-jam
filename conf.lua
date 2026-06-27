2e1db4ba327cc96786f215bbd4d6a91cfc41577a
mshj <mshj@protonmail.com>
2026-06-27 16:30:47 -0400

    boilerplate

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "High Noon"
+    t.version = "11.5"
+    t.console = true
+    t.window.width = 768
+    t.window.height = 576
+end
---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+    -- Initialize variables here
+end
+
+function love.update(dt)
+    -- Update game logic here
+end
+
+function love.draw()
+    love.graphics.print("High Noon!", 400, 300)
+end
